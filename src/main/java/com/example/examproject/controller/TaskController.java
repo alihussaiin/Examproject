596f38f7b2156d318a1f0ad83dc7e3f2100c09d2
alihussaiin <alhu0002@stud.kea.dk>
2024-05-28 16:11:02 +0200

    crud virker på alle

--- src/main/java/com/example/examproject/controller/TaskController.java
+++ src/main/java/com/example/examproject/controller/TaskController.java
@@ -36,11 +36,12 @@ public class TaskController {
     }
 
 
-    @GetMapping("/tasks/{subprojectId}")
-    public String getAllTasks(@PathVariable("subprojectId") int subprojectId, Model model) {
+    @GetMapping("/tasks/{subprojectId}/{project_id}")
+    public String getAllTasks(@PathVariable("subprojectId") int subprojectId, @PathVariable ("project_id") int project_id, Model model) {
         List<Task> tasks = taskService.getAllTasks(subprojectId);
         model.addAttribute("tasks", tasks);
         model.addAttribute("subprojectId", subprojectId);
+        model.addAttribute("project_id", project_id);
         return "tasks";
     }
 
